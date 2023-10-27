008daaaf14516b3130726ed09b08958411ec8335
Konrad Szymańczyk <123763212+Veiroo@users.noreply.github.com>
2023-10-27 11:41:16 +0200

    Delete newproject directory

--- newproject/js/scrip.js
+++
@@ -1,6 +0,0 @@
-let button = document.querySelector(".button");
-let background = document.querySelector(".backgroundColor");
-
-button.addEventListener("click", () => {
-    background.classList.toggle("backColor")
-});
